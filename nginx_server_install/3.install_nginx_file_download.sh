dc121fddc2fa5cb328317f077a2448339f79edf9
章刚 <370637594@qq.com>
2020-05-28 14:40:23 +0800

    更新脚本

--- nginx_server_install/3.install_nginx_file_download.sh
+++ nginx_server_install/3.install_nginx_file_download.sh
@@ -2,14 +2,15 @@
 
 ############################################################################
 #   author:zhanggang 370637594@qq.com
-#   desc: nginx 作为 server
+#   desc: nginx 作为 文件下载服务器
 #   date: 2020年4月14日10:58:05
 ############################################################################
 
+
 #镜像名称
-docker_name="nginx_server"
+docker_name="nginx_download_server"
 #镜像映射的端口
-port="80"
+port="9111"
 
 #1.删除容器
 function del_image() {
@@ -20,8 +21,19 @@ function del_image() {
 function run_image() {
     docker run -d --restart=on-failure:5 \
     -p $port:80 \
+    -v $PWD/download:/usr/share/nginx/html \
     --name $docker_name nginx:1.13
+}
+
+#3.修改配置文件和重启服务
+function modify_conf() {
+    docker cp download/default.conf $docker_name:/etc/nginx/conf.d
+    docker restart $docker_name
 }
 
 del_image
 run_image
+modify_conf
+
+echo "把文件放在download文件夹下 访问 http://ip:port即可"
+
